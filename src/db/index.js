df521237509deb53d35d08bbb3433c8574783b04
preksha <jainpreksha2712@gmail.com>
2024-04-15 07:25:49 +0530

    update : export client

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { MONGODB_URI } from "../config/server.config.js";
 
 const uri = `${MONGODB_URI}`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
+export const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
@@ -25,7 +25,7 @@ export async function connectDB() {
     );
 
     dbConnection = database;
-  } finally {
+  } catch (error) {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
